ecd270624e553981397ab224722012d7016d440d
Suresh <surdhana@in.ibm.com>
2021-07-09 17:33:52 +0530

    some documentation

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,3 +1,6 @@
+// This file meant to keep all constants such as db connection, api key and other system sensitive information
+// In actual project this will be derived from env file
+
 export const FLICKR_API_KEY = 'c288e8253cb6b55fe74966a0b036d702';
 export const FLICKR_ENDPOINT = 'https://www.flickr.com/services/rest/';
 export const FLICKR_FORMAT = 'json';
